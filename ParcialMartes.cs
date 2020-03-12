3b51b7d7d4509e2c813ab1d31f921bc08e1ac640
jordanbolivarq <60189623+jordanbolivarq@users.noreply.github.com>
2020-03-11 22:30:03 -0500

    Add files via upload

---
+++ ParcialMartes.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+class Parcial {
+
+    string[] names;
+    double[] data;
+
+
+    public Parcial(string[] _names, double[] _data) {
+        data = new double[_data.Length];
+        _data.CopyTo(data, 0);
+        names = new string[_names.Length];
+        _names.CopyTo(names, 0);
+    }
+
+
+    public int PrimerPunto() {
+        string[] nombres = new string[names.Length]; names.CopyTo(nombres, 0);
+        double[] notas = new double[data.Length]; data.CopyTo(notas, 0);
+
+        int salida = 0;
+        //--------------------------------------------
+        //- Abajo de esta línea va su código ---------
+
+        for (int j = 0; j < notas.Length; j++) { // ORDENAMIENTO
+            for (int i = 0; i < notas.Length - 1; i++) {
+                if (notas[i] > notas[i + 1]) {
+
+                    double tmp1 = notas[i];
+                    notas[i] = notas[i + 1];
+                    notas[i + 1] = tmp1;
+
+                    string tmp2 = nombres[i];
+                    nombres[i] = nombres[i + 1];
+                    nombres[i + 1] = tmp2;
+                }
+            }
+        }
+
+        double mediana = 0;
+        if (notas.Length % 2 != 0) { // IMPAR
+            mediana = notas[(int)(notas.Length / 2)];
+        } else { // PAR
+            mediana = (notas[(int)(notas.Length / 2)] + notas[(int)(notas.Length / 2) - 1]) / 2;
+        }
+
+
+        for (int i = 0; i < notas.Length; i++) {
+            if (notas[i] >= mediana + 0.5) {
+                salida++;
+            }
+        }
+
+        //- Arriba de esta línea va su código --------
+        return salida;
+    }
+
+    public string[] SegundoPunto() {
+        string[] nombres = new string[names.Length]; names.CopyTo(nombres, 0);
+        double[] notas = new double[data.Length]; data.CopyTo(notas, 0);
+
+        string[] salida = new string[5];
+        //--------------------------------------------
+        //- Abajo de esta línea va su código ---------
+
+        // MEDIANA
+        for (int j = 0; j < notas.Length; j++) { // ORDENAMIENTO
+            for (int i = 0; i < notas.Length - 1; i++) {
+                if (notas[i] > notas[i + 1]) {
+
+                    double tmp1 = notas[i];
+                    notas[i] = notas[i + 1];
+                    notas[i + 1] = tmp1;
+
+                    string tmp2 = nombres[i];
+                    nombres[i] = nombres[i + 1];
+                    nombres[i + 1] = tmp2;
+                }
+            }
+        }
+
+        double mediana = 0;
+        if (notas.Length % 2 != 0) { // IMPAR
+            mediana = notas[(int)(notas.Length / 2)];
+        } else { // PAR
+            mediana = (notas[(int)(notas.Length / 2)] + notas[(int)(notas.Length / 2) - 1]) / 2;
+        }
+        // FIN MEDIANA
+
+        double[] distancias = new double[notas.Length];
+        for (int i = 0; i < notas.Length; i++) {
+            distancias[i] = Math.Abs(notas[i] - mediana);
+        }
+
+        for (int j = 0; j < notas.Length; j++) { // ORDENAMIENTO
+            for (int i = 0; i < notas.Length - 1; i++) {
+                if (distancias[i] > distancias[i + 1]) {
+
+                    double tmp1 = distancias[i];
+                    distancias[i] = distancias[i + 1];
+                    distancias[i + 1] = tmp1;
+
+                    string tmp2 = nombres[i];
+                    nombres[i] = nombres[i + 1];
+                    nombres[i + 1] = tmp2;
+                }
+            }
+        }
+
+        for (int i = 0; i < salida.Length; i++) {
+            salida[i] = nombres[i];
+        }
+
+
+        //- Arriba de esta línea va su código --------
+        return salida;
+    }
+
+    public string[] TercerPunto() {
+        string[] nombres = new string[names.Length]; names.CopyTo(nombres, 0);
+        double[] notas = new double[data.Length]; data.CopyTo(notas, 0);
+
+        string[] salida = new string[0];
+        //--------------------------------------------
+        //- Abajo de esta línea va su código ---------
+
+
+
+
+        //- Arriba de esta línea va su código --------
+        return salida;
+    }
+
+    public string[] CuartoPunto() {
+        string[] nombres = new string[names.Length]; names.CopyTo(nombres, 0);
+        double[] notas = new double[data.Length]; data.CopyTo(notas, 0);
+
+        string[] salida = new string[0];
+        //--------------------------------------------
+        //- Abajo de esta línea va su código ---------
+
+
+
+        //- Arriba de esta línea va su código --------
+        return salida;
+    }
+
+    public List<string> TercerPuntoListas() {
+        string[] nombres = new string[names.Length]; names.CopyTo(nombres, 0);
+        double[] notas = new double[data.Length]; data.CopyTo(notas, 0);
+
+        List<string> salida = new List<string>();
+        //--------------------------------------------
+        //- Abajo de esta línea va su código ---------
+
+        //nota >= 0.5 x promedio + 1.5
+        double total = 0, promedio = 0;
+        for (int i = 0; i < notas.Length; i++) {
+            total += notas[i];
+        }
+        promedio = total / notas.Length;
+        for (int i = 0; i < notas.Length; i++) {
+            if( notas[i] >= 0.5 * promedio +1.5){
+                salida.Add(nombres[i]);
+            }
+        }
+        //- Arriba de esta línea va su código --------
+        return salida;
+    }
+
+    public List<string> CuartoPuntoListas() {
+        string[] nombres = new string[names.Length]; names.CopyTo(nombres, 0);
+        double[] notas = new double[data.Length]; data.CopyTo(notas, 0);
+
+        List<string> salida = new List<string>();
+        //--------------------------------------------
+        //- Abajo de esta línea va su código ---------
+        double total = 0, promedio = 0;
+        for (int i = 0; i < notas.Length; i++) {
+            total += notas[i];
+        }
+        promedio = total / notas.Length;
+
+        for (int i = 0; i < nombres.Length; i++) { // RECORRER LOS NOMBRES
+            int contador = 0;
+            for (int j = 0; j < nombres[i].Length; j++) { // RECORRER LAS LETRAS
+                if (nombres[i][j] == 'A') {
+                    contador ++;
+                }
+            } // FIN RECORRER LETRAS
+
+            if (contador == 0 && notas[i] >= 4) { // SE ALMACENAN EN LA SALIDA
+                salida.Add(nombres[i]);
+            }
+        }
+        //- Arriba de esta línea va su código --------
+        return salida;
+    }
+}
+
+
